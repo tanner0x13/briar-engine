87f793a6d31258318a7feb1585911b092ecaa42b
Tanner Zigrang <tanner.zigrang@gmail.com>
2024-03-03 16:40:54 -0800

    initial commit of python script that will serve as engine build system

---
+++ scripts/build.py
@@ -0,0 +1,23 @@
+"""
+Script to compile the engine src
+"""
+
+####
+# Imports
+####
+import sys
+
+
+
+####
+# Main Function
+####
+def main() -> int:
+    return 0
+
+
+####
+# Script Entry
+####
+if __name__ == '__main__':
+    sys.exit(main())
